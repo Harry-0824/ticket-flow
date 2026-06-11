d51f8af0e7941cbe999385b5726add1e65ea5ecc
Harry-0824 <prince11290824@gmail.com>
2026-06-11 22:29:49 +0800

    Initialize frontend skeleton

---
+++ frontend/src/views/HomeView.vue
@@ -0,0 +1,9 @@
+<template>
+  <section class="page">
+    <h1>TicketFlow</h1>
+    <p>
+      Frontend skeleton for the TicketFlow MVP. Ticket screens and API integration
+      will be added in later issues.
+    </p>
+  </section>
+</template>
